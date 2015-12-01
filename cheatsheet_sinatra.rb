21b6e30bb25fa36e2fd235fce56318819060a729
Amy Eisenmenger <amy.eisenmenger@gmail.com>
2015-11-30 18:16:02 -0700

    initial commit

---
+++ cheatsheet_sinatra.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'pry'
+
+def search(cmd)
+  command= cmd
+  puts `man #{command}`
+end
+
+get '/' do
+  erb :index
+end
+
+get '/command_line' do
+  @cmd =['cp', 'ls', 'mkdir', 'touch', 'mv']
+  erb :command_line
+end
+
+get '/search' do
+  erb :search
+end
+
+get '/command_result' do
+  search(params[:cmd])
+  erb :command_result
+end
